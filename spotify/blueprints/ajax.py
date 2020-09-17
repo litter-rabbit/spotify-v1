368d8ff6d84d37c971b579a527ee734d22d386b7
litter-rabbit <709343607@qq.com>
2020-09-17 10:14:59 +0800

    更新时间

--- spotify/blueprints/ajax.py
+++ spotify/blueprints/ajax.py
@@ -5,6 +5,10 @@ from multiprocessing import Process
 from spotify.extendtions import db
 from spotify.parse import get
 
+from datetime import timedelta
+import datetime
+
+
 ajax_bp = Blueprint('ajax', __name__)
 
 
@@ -72,9 +76,12 @@ def new_links():
 
 @ajax_bp.route('/get/orders', methods=['POST', 'GET'])
 def get_orders():
+    now = datetime.datetime.utcnow()
+    new_now = datetime.datetime(now.year, now.month, now.day, 15, 59, 59)
     un_orders = Order.query.filter(Order.status != '处理成功').order_by(Order.timestamp.desc()).all()
-    orders_competed = Order.query.filter(Order.status == '处理成功').order_by(Order.timestamp.desc()).limit(
-        current_app.config['PER_PAGE'])
+    orders_competed = Order.query.filter(Order.timestamp >= new_now - timedelta(days=1)).filter(
+        Order.status == '处理成功').order_by(Order.timestamp.desc()).limit(current_app.config['PER_PAGE'])
+
     return render_template('main/orders.html', orders=un_orders, orders_competed=orders_competed)
 
 
